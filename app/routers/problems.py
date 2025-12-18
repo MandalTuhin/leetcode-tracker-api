9e96759d6f1ba96466ae1204acf1f9230d1bb3a7
Tuhin Mandal <73009536+MandalTuhin@users.noreply.github.com>
2025-12-18 13:17:48 +0530

    refactor(types): import types to problems route for use

--- app/routers/problems.py
+++ app/routers/problems.py
@@ -1,14 +1,21 @@
 from fastapi import APIRouter
 from app.models.problem import Problem
+from app.core.types import unordered_map
 
 router = APIRouter(prefix="/problems", tags=["Problems"])
 
 db: list[Problem] = []
+db: unordered_map[int, Problem] = {}
 
 @router.post("/")
 async def add_problem(problem: Problem):
     db.append(problem)
+    db[problem.id] = problem
     return {"message" : "Problem added!", "count": len(db)}
+
+# @router.get("/")
+# async def get_problems() -> unordered_map[int, Problem]:
+#     return db
 
 @router.get("/")
 async def get_problems() -> list[Problem]:
