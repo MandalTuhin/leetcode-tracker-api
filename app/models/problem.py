64ea13bd1c8c37f2635ab17c0070ec947a7b9f47
Tuhin Mandal <73009536+MandalTuhin@users.noreply.github.com>
2025-12-18 12:38:30 +0530

    feat(model): add Enum to problem difficulty to restrict values

--- app/models/problem.py
+++ app/models/problem.py
@@ -1,8 +1,14 @@
 from pydantic import BaseModel
+from enum import Enum
+
+class Diffiulty(str, Enum):
+    EASY = "Easy"
+    MEDIUM = "Medium"
+    DIFFICULT = "Diffiult" # I am adding "difficult" for now, I may change it later.
 
 class Problem(BaseModel):
     id: int
     title: str
-    difficulty: str
+    difficulty: Diffiulty # Now restricted to the Enum values
     tags: list[str] = []
     solution_cpp: str | None = None
